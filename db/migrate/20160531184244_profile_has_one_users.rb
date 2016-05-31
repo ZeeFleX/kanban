20106e2b33daf7871e1b7870b58d8d76f14dc617
ZeeFleX <kardapolov@gmail.com>
2016-05-31 23:58:24 +0500

    Added profiles model and link (has_one) between profiles and users

---
+++ db/migrate/20160531184244_profile_has_one_users.rb
@@ -0,0 +1,5 @@
+class ProfileHasOneUsers < ActiveRecord::Migration
+  def change
+  	add_column :profiles, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160529083128) do
+ActiveRecord::Schema.define(version: 20160531184244) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name",       limit: 255, null: false
+    t.string   "address",    limit: 255
+    t.string   "logo",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "firstname",  limit: 255
+    t.string   "lastname",   limit: 255
+    t.string   "middlename", limit: 255
+    t.datetime "birthdate"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "user_id",    limit: 4
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",   limit: 255
